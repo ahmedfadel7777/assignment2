5230c07211efc037e78a3096dd2cd60ce2f833e0
AhmedGhanem7777 <ahmedfadeghanem012@gmail.com>
2024-07-17 06:31:23 +0300

    second commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home/home.component';
+import { NotFoundComponent } from './not-found/not-found/not-found.component';
+import { AboutUsComponent } from './about/about-us/about-us.component';
+import { BlogComponent } from './blog/blog/blog.component';
+import { ContactComponent } from './contact/contact/contact.component';
+import { EducationComponent } from './education/education/education.component';
+import { ServicesComponent } from './services/services/services.component';
+import { SkillsComponent } from './skills/skills/skills.component';
+import { WorkComponent } from './work/work/work.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:'home',pathMatch:'full'},
+  {path:'home',component:HomeComponent},
+  {path:'about',component:AboutUsComponent},
+  {path:'blog',component:BlogComponent},
+  {path:'contact',component:ContactComponent},
+  {path:'education',component:EducationComponent},
+  {path:'services',component:ServicesComponent},
+  {path:'skills',component:SkillsComponent},
+  {path:'work',component:WorkComponent},
+  {path:'**',component:NotFoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeModule } from './home/home.module';
+import { SideBarComponent } from './side-bar/side-bar/side-bar.component';
+import { SideBarModule } from './side-bar/side-bar.module';
+import { AboutModule } from './about/about.module';
+import { BlogModule } from './blog/blog.module';
+import { ContactModule } from './contact/contact.module';
+import { EducationModule } from './education/education.module';
+import { ServicesModule } from './services/services.module';
+import { SkillsModule } from './skills/skills.module';
+import { WorkModule } from './work/work.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +20,16 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HomeModule,
+    SideBarModule,
+    AboutModule,
+    BlogModule,
+    ContactModule,
+    EducationModule,
+    ServicesModule,
+    SkillsModule,
+    WorkModule
   ],
   providers: [],
   bootstrap: [AppComponent]
